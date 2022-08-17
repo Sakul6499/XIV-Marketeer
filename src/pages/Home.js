f1143d918f1072f09c34459c64c8f3424e3d9c83
Lukas Weber <me@sakul6499.de>
2022-08-17 21:24:01 +0200

    Made Home use new format

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,32 @@
-import { Link } from "react-router-dom";
+// React
+import React from 'react';
+
+// Components
 import Side from "../components/Side";
 
-function Home() {
-    return (
-        <>
-            <main className="columns">
-                <div className="column is-one-fifth">
-                    <Side />
-                </div>
-                <div className="column">
-                    <section className="hero">
-                        <div className="hero-body">
-                            <p className="title">
-                                Welcome to XIV Marketeer!
-                            </p>
-                            <p className="subtitle">
-                                Select a category from the left to get started.
-                            </p>
-                        </div>
-                    </section>
-                </div>
-            </main>
-        </>
-    );
+class Home extends React.Component () {
+    render() {
+        return (
+            <>
+                <main className="columns">
+                    <div className="column is-one-fifth">
+                        <Side />
+                    </div>
+                    <div className="column">
+                        <section className="hero">
+                            <div className="hero-body">
+                                <p className="title">
+                                    Welcome to XIV Marketeer!
+                                </p>
+                                <p className="subtitle">
+                                    Select a category from the left to get started.
+                                </p>
+                            </div>
+                        </section>
+                    </div>
+                </main>
+            </>
+        );
+    }
 }
 export default Home;
